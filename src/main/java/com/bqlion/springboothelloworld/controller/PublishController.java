ca7526ea036ee28e3007e0481090acee8988a676
Bq Lion <398703917@qq.com>
2019-07-27 23:45:37 +0800

    sql bug

--- src/main/java/com/bqlion/springboothelloworld/controller/PublishController.java
+++ src/main/java/com/bqlion/springboothelloworld/controller/PublishController.java
@@ -1,16 +1,76 @@
 package com.bqlion.springboothelloworld.controller;
 
+import com.bqlion.springboothelloworld.mapper.QuestionMapper;
+import com.bqlion.springboothelloworld.mapper.UserMapper;
+import com.bqlion.springboothelloworld.model.Question;
+import com.bqlion.springboothelloworld.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+
 
 /* *
  * Created by BqLion on 2019/7/24
  */
 @Controller
 public class PublishController {
-    @GetMapping("/publish")
-    public String publish(){
-        return "publish";
 
+    @Autowired
+    private QuestionMapper QuestionMapper;
+
+    @Autowired
+    private UserMapper UserMapper;
+
+    @GetMapping("/publish")
+    public String publish() {
+        return "publish";
+    }
+
+    @PostMapping("/publish")
+    public String doPublish(
+            @RequestParam("title") String title,
+            @RequestParam("description") String description,
+            @RequestParam("tag") String tag,
+            HttpServletRequest request,
+            Model model) {
+        model.addAttribute("title", title);
+        model.addAttribute("description", description);
+        model.addAttribute("tag", tag);
+
+        User user = null;
+        Cookie[] cookies = request.getCookies();
+        if (cookies != null) {
+            for (Cookie cookie : cookies) {
+                if (cookie.getName().equals("token")) {
+                    String token = cookie.getValue();
+
+                    user = UserMapper.findByToken(token);
+                    if (user != null) {
+                        request.getSession().setAttribute("user", user);
+                    }
+                    break;
+                }
+            }
+        }
+        if (user == null) {
+            model.addAttribute("error", "用户未登录");
+            return "publish";
+        }
+
+        Question question = new Question();     //question对象接受各种数据
+        question.setTitle(title);
+        question.setDescription(description);
+        question.setTag(tag);
+        question.setCreator(user.getId());
+        question.setGmt_create(System.currentTimeMillis());
+        question.setGmt_modified(question.getGmt_create());
+        QuestionMapper.create(question);          //mapper把数据写入数据库
+        return "redirect:/";                //发布成功就返回首页
     }
 }
---
+++ src/main/java/com/bqlion/springboothelloworld/mapper/QuestionMapper.java
@@ -0,0 +1,15 @@
+package com.bqlion.springboothelloworld.mapper;
+
+/* *
+ * Created by BqLion on 2019/7/26
+ */
+
+import com.bqlion.springboothelloworld.model.Question;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface QuestionMapper {
+    @Insert("insert into (title,description,gmt_create,gmt_modified,creator,tag) values (#{title},#{description},#{gmtCreate},#{gmtModified},#{creator},#{tag})")
+    void create(Question question);
+}
