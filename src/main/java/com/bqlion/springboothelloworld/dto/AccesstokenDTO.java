6ac1c17629e4f2f83698996a1832b7ab7b7c9485
Bq Lion <398703917@qq.com>
2019-08-19 10:35:33 +0800

    fix redirectUrl

--- src/main/java/com/bqlion/springboothelloworld/dto/AccesstokenDTO.java
+++ src/main/java/com/bqlion/springboothelloworld/dto/AccesstokenDTO.java
@@ -8,6 +8,6 @@ public class AccesstokenDTO {
     private String client_id;
     private String client_secret;
     private String code;
-    private String redirect_uri;
+    private String redirectUri;
     private String state;
 }
